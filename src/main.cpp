f954a0673c4518c1e79fbc9b754c7c489fd6cd1f
Kanav <kanav@Kanavs-MacBook-Air.local>
2026-07-25 02:41:16 +0530

    added the echo feature with substr

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,10 @@ while(true)
   // TODO: Uncomment the code below to pass the first stage
   std::cout << "$ ";
 //what i need to do is take input from the user
-
+// i want to add stage 5 a feature that would help me to echo the arguments given in propmpt
   std::string command;
 
   if(!std::getline(std::cin,command))
-  {
-break;
-  }
-
-  if(command=="exit"||command=="EXIT"||command=="Exit")
   {
 break;
   }
@@ -26,6 +21,23 @@ break;
   {
     continue;
   }
+  if(command=="exit"||command=="EXIT"||command=="Exit")
+  {
+break;
+  }
+  std::size_t start=0;
+  std::size_t end=command.find(" ",start);
+  std::string word=command.substr(start,end);
+  start=end+1;
+
+  if(word=="echo")
+  {
+      std::string argument=command.substr(start);
+      std::cout<<argument<<'\n';
+      continue;
+      }
+    
+  
   std::cout << command <<": command not found"<< std:: endl;
 }
 
